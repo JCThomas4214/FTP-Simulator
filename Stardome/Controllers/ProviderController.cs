cbe2a0652db86be735dc93e4b927935cf5ccc541
chandyt <timmychandy@hotmail.com>
2015-02-09 13:48:00 -0500

    Added controller and views for providers and clients

---
+++ Stardome/Controllers/ProviderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stardome.Controllers
+{
+    public class ProviderController : Controller
+    {
+        //
+        // GET: /Provider/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
