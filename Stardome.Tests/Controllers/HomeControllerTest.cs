2ea83de6202b20462022f063003d0064a1f0cea8
sabrina-gisselle <sabrina.gisselle@ufl.edu>
2015-02-16 09:46:14 -0500

    87570978 comment# UnitTests

--- Stardome.Tests/Controllers/HomeControllerTest.cs
+++ Stardome.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Stardome;
 using Stardome.Controllers;
 
 namespace Stardome.Tests.Controllers
@@ -13,39 +8,39 @@ namespace Stardome.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void Users()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = controller.Users() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("User Management Page", result.ViewBag.Message);
         }
 
         [TestMethod]
-        public void About()
+        public void Content()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            ViewResult result = controller.Content() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
-        public void Contact()
+        public void Settings()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            ViewResult result = controller.Settings() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
---
+++ Stardome.Tests/Services/AesEncryptionTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stardome.Services.Application;
+
+namespace Stardome.Tests.Services
+{
+    [TestClass]
+    public class AesEncryptionTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void AesEncrypt_plainTextIsNull()
+        {
+            // Arrange and Act
+            string result = AesEncryption.Encrypt(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void AesEncrypt_plainTextIsEmpty()
+        {
+            // Arrange and Act
+            string result = AesEncryption.Encrypt("");
+        }
+
+        [TestMethod]
+        public void AesEncrypt_plainTextIsTest()
+        {
+            // Arrange 
+            string plainText = "Test";
+
+            // Act
+            string result = AesEncryption.Encrypt(plainText);
+
+            // Assert
+            Assert.IsTrue(result.Equals("Rqx7DMiEIVI+jbIypf+7RA=="));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void AesDecrypt_cipherTextIsNull()
+        {
+            // Arrange and Act
+            string result = AesEncryption.Decrypt(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void AesDecrypt_cipherTextIsEmpty()
+        {
+            // Arrange and Act
+            string result = AesEncryption.Decrypt("");
+        }
+
+        [TestMethod]
+        public void AesDecrypt_cipherTextIsTest()
+        {
+            // Arrange 
+            string cipherText = "Rqx7DMiEIVI+jbIypf+7RA==";
+
+            // Act
+            string result = AesEncryption.Decrypt(cipherText);
+
+            // Assert
+            Assert.IsTrue(result.Equals("Test"));
+        }
+    }
+}
