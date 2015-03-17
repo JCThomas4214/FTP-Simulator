469a1e41182b4d6772f63bd45a112b75acf72a55
Kevin Cheddar <kevincheddar@gmail.com>
2015-03-17 02:45:59 -0400

    Attempted multiple file upload

--- Stardome/Controllers/ProducerController.cs
+++ Stardome/Controllers/ProducerController.cs
@@ -5,18 +5,20 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
 using System.Web;
 using System.Web.Mvc;
 using WebMatrix.WebData;
 
+
 namespace Stardome.Controllers
 {
+    
     
     public class ProducerController : Controller
     {
         //
         // GET: /Producer/
+       
 
         private IUserAuthCredentialService userAuthCredentialService;
         private IRoleService roleService;
@@ -44,20 +46,26 @@ namespace Stardome.Controllers
             }
 
         }
-        [HttpPost]
-        public ActionResult Upload(IEnumerable<HttpPostedFileBase> files)
-        {
-           
-            var allowedExtensions = new[] { ".doc", ".mp3", ".txt", ".jpeg" };
 
-            foreach (var file in files)
+        
+
+        [HttpPost]
+        public ActionResult Upload(IEnumerable<HttpPostedFileBase> files, string selectPath)
+        {
+            List<string> results = new List<string>();
+            var allowedExtensions = new[] { ".doc", ".mp3", ".txt", ".jpeg" };
+            var uploadedFiles = 0;
+            var existingFiles = 0;
+            var incorrectFiles = 0;
+            foreach (var file in files) {
                 if (file != null && file.ContentLength > 0)
                 {
                     var extension = Path.GetExtension(file.FileName);
                     if (!allowedExtensions.Contains(extension))
                     {
-                        ViewBag.Message = "Incorrect file type";
+                        //ViewBag.Message = "Incorrect file type";
                         //Files with an extension that we don't allow, won't be uploaded
+                        ++incorrectFiles;
                     }
 
 
@@ -71,11 +79,13 @@ namespace Stardome.Controllers
                             if (!System.IO.File.Exists(path))
                             {
                                 file.SaveAs(path);
-                                ViewBag.Message = "File uploaded successfully";
+                                //ViewBag.Message = "File uploaded successfully";
+                                ++uploadedFiles;
                             }
                             else
                             {
-                                ViewBag.Message = "A file with that name already exists";
+                                //ViewBag.Message = "At least 1 file already exists. Please rename it to upload it.";
+                                ++existingFiles;
                             }
 
                         }
@@ -83,13 +93,43 @@ namespace Stardome.Controllers
                         {
                             ViewBag.Message = "ERROR:" + ex.Message.ToString();
                         }
-                }
+                }}
+                //else Testing without Else
+                //{
+                   // ViewBag.Message = "You have not specified any files.";
+                //}
+            if (existingFiles > 0)
+            {
+                if (existingFiles == 1)
+                    ViewBag.Existing = "1 file already exists. Please rename it.";
+                    //results.Add(String.Format("1 file already exists. Please rename it."));
                 else
-                {
-                    ViewBag.Message = "You have not specified a file.";
-                }
-            
-            return View();
+                    ViewBag.Existing = existingFiles + " files already exist. Please rename them.";
+                    //results.Add(String.Format(existingFiles + " files already exist. Please rename them."));
+                                     
+            }
+
+            if (incorrectFiles > 0)
+            {
+                if (incorrectFiles == 1)
+                    ViewBag.Incorrect = " 1 file is of the wrong type.";
+                    //results.Add(String.Format(" 1 file is of the wrong type."));
+                else
+                    ViewBag.Incorrect = incorrectFiles + " files are of the wrong type.";
+                    //results.Add(String.Format(incorrectFiles + " files are of the wrong type."));
+            }
+
+            if (uploadedFiles == 1)
+                ViewBag.Uploaded = " 1 file uploaded successfully.";
+            //results.Add(String.Format(" 1 file uploaded successfully."));
+            else
+                ViewBag.Uploaded = uploadedFiles + " files uploaded successfully.";
+                //results.Add(String.Format(uploadedFiles + " files uploaded successfully."));
+            //string[] resultArray = results.ToArray();
+            //ViewBag.Results = resultArray;
+             
+            return View(results);
+            //return View(results);
         }
 
         public ActionResult Upload()
